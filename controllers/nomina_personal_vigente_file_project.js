9272416df9344812ba6ced0ecafd71a5afb85a75
Jean Pierre <jpena.suazo@gmail.com>
2023-07-16 12:51:02 -0400

    añade calendario asistencia para proceso

--- controllers/nomina_personal_vigente_file_project.js
+++ controllers/nomina_personal_vigente_file_project.js
@@ -144,8 +144,8 @@ res.render("../views/nomina_bancaria", { nomina: infoPersonas,datosNomina:datosN
 
 async function getCalendarioAsistencias (req,res) {
   let empresa=0
-  let mes='2022-11-01'
-  let cenco2_codi='923-023'
+  let mes='2023-01-01'
+  let cenco2_codi='923-001'
 
   var options = {
     format: 'Letter',
@@ -465,6 +465,24 @@ var StatusNominasBancariasTemplate = {
 
 var StatusNominasBancarias = JSON.parse(JSON.stringify(StatusNominasBancariasTemplate))
 
+
+
+
+
+//Calendario Asistencia
+
+var StatusCalendarioAsistenciaTemplate = {
+
+  isExecuting: false,
+  percent: 0,
+  msgs: []
+}
+
+var StatusCalendarioAsistencia = JSON.parse(JSON.stringify(StatusCalendarioAsistenciaTemplate))
+
+
+
+
 /** 
  * Funcion del socket
  * @constructor
@@ -474,8 +492,8 @@ var StatusNominasBancarias = JSON.parse(JSON.stringify(StatusNominasBancariasTem
 
 
 io.on('connection', (socket) => {
+  socket.emit('getStatusCalendarioAsistencia', StatusCalendarioAsistencia)
 
-  socket.emit('getStatusNominasBancarias', StatusNominasBancarias)
 
   socket.on('getTest', async (uploadFileName) => {
   })
@@ -486,235 +504,157 @@ io.on('connection', (socket) => {
 
   });
   
-    socket.on('getNominasBancarias', async (data) => {
+    
 
-      let empresa=data.empresa
-      let mes=data.mes
-      let fechaPago=data.fechaPago
-      let variableBase=data.codigoNomina
+
+
+
+    socket.on('getCalendarioAsistencia', async (empresa,mes) => {
+
+      //////////////////////////////////NUEVO PROCESO
   
       //data trae la info del mes y la empresa del proceso
-      console.log("se empieza a ejecutar proceso Nominas: "+empresa+" "+mes+" "+fechaPago+" "+variableBase)
+      console.log("se empieza a ejecutar proceso Calendario Asistencia: " +empresa+" "+mes)
   
     
-      StatusNominasBancarias = JSON.parse(JSON.stringify(StatusNominasBancariasTemplate))
-      StatusNominasBancarias.isExecuting = true
-      StatusNominasBancarias.userParams={mes:mes,empresa:empresa}
-     // StatusNominasBancarias.userParams={mes:dataUser["mes"],empresa:dataUser["empresa"]}
+      StatusCalendarioAsistencia = JSON.parse(JSON.stringify(StatusCalendarioAsistenciaTemplate))
+      StatusCalendarioAsistencia.isExecuting = true
+      //StatusCalendarioAsistencia.userParams={mes:mes,empresa:empresa}
+     // StatusPrevired.userParams={mes:dataUser["mes"],empresa:dataUser["empresa"]}
   
-      io.emit('getStatusNominasBancarias', StatusNominasBancarias)
-      await getNominasBancarias(empresa,mes,variableBase,fechaPago)
-      StatusNominasBancarias.isExecuting = 0
-      io.emit('getStatusNominasBancarias', StatusNominasBancarias)
+      io.emit('getStatusCalendarioAsistencia', StatusCalendarioAsistencia)
+      await getCalendarioAsistenciaFileProject(empresa,mes)
+      StatusCalendarioAsistencia.isExecuting = 0
+      io.emit('getStatusCalendarioAsistencia', StatusCalendarioAsistencia)
   
     });
+
+    async function getCalendarioAsistenciaFileProject(empresa,mes){
+      console.log("todos los trabajos terminados")
+    }
+
+
+
+    async function getCalendarioAsistenciaFileProjectXXXXXXX(empresa,mes){
   
-
-
-async function getNominasBancarias (empresa,mesProceso,variableBase,fechaPago) {
-
+      let carpetaBurst='pdfBurstPrevired'
   
-//se añade control desde bd para no tener que modificar código cuando se necesite añadir otros clientes de apertura por persona
-var ConfigAperturaPersona = await sequelizeMssql  .query(` SELECT [EMP_CODI]
-,[TIPO]
-,[CENCO1_CODI]
-FROM `+constants.TABLE_NOMINAS_BANCARIAS_POR_PERSONA.database+`.dbo.`+constants.TABLE_NOMINAS_BANCARIAS_POR_PERSONA.table  
-      , { model: NominasPorPersonaModel,
-        mapToModel: true, // pass true here if you have any mapped fields
-        raw: true
-      })
-
-
-	var startTime = new Date();
-
-	let pathLogs = 'data-logs/'
-  let nameLogFile='previred'
-
-  //let variableBase='D066'
-  //let mesProceso='2021-05-01'
-  //let empresa=0
-
-  console.log("comienza el proceso nominas bancarias",empresa,mesProceso)
-
-  //datos variables nominas (fecha, nombre, etc)
-  var empresaDetalle = constants.EMPRESAS.find(x => x.ID == empresa)
-  let variableNominaDetalle=VariablesNominasBancarias.find(x=>x["COD_VARIABLE"]==variableBase)
-  console.log(variableNominaDetalle)
-
-let nombreNomina=variableNominaDetalle["NOMBRE_NOMINA"]
-let rutEmpresa=empresaDetalle["RUT"]
- let fechaHora=Utils.getDateFormat('-')
-  console.log(fechaHora)
-  //2021/06/14/23/27/38 retorna 
-
-  let fechaActual=fechaHora.substr(8,2)+"/"+fechaHora.substr(5,2)+"/"+fechaHora.substr(0,4)
-  let horaActual=fechaHora.substr(11,10).replace(/-/g, ':');
-  let fechaPagoFormat=fechaPago.substr(8,2)+"/"+fechaPago.substr(5,2)+"/"+fechaPago.substr(0,4)
-  console.log(horaActual,fechaActual,fechaPagoFormat,empresaDetalle.RUT,nombreNomina)
-  let datosNominaHeader={FECHA_ACTUAL:fechaActual,HORA_ACTUAL:horaActual,FECHA_PAGO:fechaPagoFormat,RUT_EMPRESA:rutEmpresa,NOMBRE_NOMINA:nombreNomina,VARIABLE_BASE:variableBase}
-
-
-
-
-  let dirDestino=FileServer.getDirDestinoProceso('nominabancaria',mesProceso,empresa)
-
-
-
-  
-  
-   dirDestino= FileServer.convertPath(dirDestino+"\\"+variableBase+"-["+variableNominaDetalle["NOMBRE_NOMINA"].replace(/\s/g, '-')+"]")
-    console.log(dirDestino,' carpeta subproceso nomina')
-  
-   
-  if (!dirDestino){
-    socket.emit('getGlobalAlert', {messaje:"Error, no hay acceso a carpeta de sobre laboral",type:'error'})
-    return
-  }
-
-        // crea carpeta del mes en destino, si no existe 
-        if (!fs.existsSync(dirDestino)){
-    
-          fs.mkdirSync(dirDestino,{recursive:true});
-
-          console.log("no existe carpeta, creada la carpeta del mes")
-      }else{
-        console.log("existe la carpeta, se debe respaldar el contenido ", dirDestino)
-      
-     ////BACKUP FILES  de cada directorio 
-     FileServer.backupFiles(FileServer.convertPath(dirDestino+'\\CLIENTE'),empresa)
-     FileServer.backupFiles(FileServer.convertPath(dirDestino+'\\INSTALACION'),empresa)
-     FileServer.backupFiles(FileServer.convertPath(dirDestino+'\\PERSONA'),empresa)
-   
-    
+      if(fs.existsSync(carpetaBurst))
+      {
+      //recrea carpeta Burst
+      try {
+        
+        fs.rmdirSync(carpetaBurst, { recursive: true });
+        fs.mkdirSync(carpetaBurst);
+       
+        console.log('Carpeta recreada exitosamente!');
+      } catch (err) {
+        console.error(err);
       }
-
-
-
-  let nominaPago = await sequelizeMssql
-      .query(`
-select per.ficha,valor
-FROM [SISTEMA_CENTRAL].[dbo].[sw_variablepersona] as per
-left join [SISTEMA_CENTRAL].[dbo].sw_areanegper AS area ON area.ficha = per.ficha AND '` + mesProceso + `'>= area.vigDesde and '` + mesProceso + `'< area.vigHasta and per.emp_codi=area.empresa
-where 
-not (area.codArn='001' and emp_codi=0) and not (area.codArn='005' and emp_codi=2)  and
-emp_codi='`+ empresa + `' and fecha='` + mesProceso + `'
-and codVariable='`+ variableBase + `' and valor>0
-
-`
-        , {
-
-          model: VariablesFicha,
-          mapToModel: true, // pass true here if you have any mapped fields
-          raw: true
-        })
-
-           //añadiremos campo para normalizar ñ
-    nominaPago=nominaPago.map(x=>{
-      x["ficha"]=x["ficha"].replace(/Ñ/g, 'N')
-      return x
-    })
-
-  console.log(nominaPago.length)
-
-            //si no existen fichas, se termina el proceso
-            if (nominaPago.length==0){
-              console.log("no hay fichas,termina el proceso")
-              socket.emit('getGlobalAlert', {messaje:"Error, no hay data para el proceso",type:'error'})
-          
   
-             //emitir mensaje de error
-              //termina el promise
-              //sale de la funcion, si no hay return, continua ejecutando lo siguiente
-             return
-            }
-
-
-
-
-let fichasNominaPago = nominaPago.map(x => { return x.ficha }).filter(unique)//.slice(0, 3)
-
-let infoPersonas = (await SoftlandController.getFichasInfoPromiseMes(fichasNominaPago, empresa, mesProceso))
-
-//añadiremos campo para realizar agrupacion en archivos este es nombres + cenco2_codi
-infoPersonas=infoPersonas.map(x=>{
-  x["NOMBRES_CENCO2_CODI"]=x["NOMBRES"]+"-["+x["CENCO2_CODI"]+"]"
-  x["FICHA"]=x["FICHA"].replace(/Ñ/g, 'N')
-  return x
-})
-
-//quita personas con rol privado y ordena por nombre
- infoPersonas=infoPersonas.filter(x=>x["ROL_PRIVADO"]=='N').sort((a, b) => (a["NOMBRES_ORD"] > b["NOMBRES_ORD"]) ? 1 : -1)
-
-infoPersonas=infoPersonas.map(persona=>{
-  persona["MEDIO_PAGO"]=''
-  persona["OF_DESTINO"]='Central'
-  persona["BANCO_GLOSA"]=''
-
-if (persona["COD_BANCO_SUC"]=='01'|| persona["COD_BANCO_SUC"]=='02'){
-  persona["MEDIO_PAGO"]='Abono en Bancuenta Credichile' 
-  persona["BANCO_GLOSA"]='BANCO DE CHILE'
-}
-else if(persona["COD_BANCO_SUC"]=='04'){
-  persona["BANCO_GLOSA"]='BANCO DEL ESTADO DE CHILE'
-  persona["MEDIO_PAGO"]='Abono en Cta. Cte. de otros' 
-}
-else if(persona["COD_TIP_EFE"]=='002'){
-  persona["MEDIO_PAGO"]='Pago Efectivo Servipag'
-
-}else{
-  persona["MEDIO_PAGO"]='Abono en Cta. Cte. de otros' 
-  persona["BANCO_GLOSA"]= persona["BANCO_DESC"]
-}
-//console.log("persona",persona)
-persona["MONTO"]=nominaPago.find(x=>x["ficha"]==persona["FICHA"])["valor"]
-
-return persona
-
-})
-   
-   let sumNomina= nominaPago.reduce((sum, b) => { return sum + parseInt(b.valor) }, 0);
-   let cantRegistros=nominaPago.length
-   let datosNomina={MONTO_TOTAL:sumNomina,NUM_REGISTROS:cantRegistros}
-
-             console.log("sum nomina",sumNomina)
-
-
-//let dirDestinoCliente=dirDestino+"/"+variableBase+"-["+variableNominaDetalle["NOMBRE_NOMINA"].replace(/\s/g, '-')+"]"+ "/CLIENTE"
-//let dirDestinoInstalacion=dirDestino+"/"+variableBase+"-["+variableNominaDetalle["NOMBRE_NOMINA"].replace(/\s/g, '-')+"]"+ "/INSTALACION"
-//let dirDestinoPersona=dirDestino+"/"+variableBase+"-["+variableNominaDetalle["NOMBRE_NOMINA"].replace(/\s/g, '-')+"]"+ "/PERSONA"
-
-let dirDestinoCliente=FileServer.convertPath( dirDestino+"\\CLIENTE")
-let dirDestinoInstalacion=FileServer.convertPath(dirDestino+"\\INSTALACION")
-let dirDestinoPersona=FileServer.convertPath(dirDestino+"\\PERSONA")
-    fs.mkdirSync(dirDestinoCliente,{recursive:true});
-
-  await generaFiles(infoPersonas,'CENCO1_CODI',dirDestinoCliente,empresa,datosNominaHeader)
-
-
-  fs.mkdirSync(dirDestinoInstalacion,{recursive:true});
-  await generaFiles(infoPersonas,'CENCO2_CODI',dirDestinoInstalacion,empresa,datosNominaHeader)
-
-
- fs.mkdirSync(dirDestinoPersona,{recursive:true});
- 
- //se añade filtro de cliente  empresas o iinstalaciones que tineen que tener apertura de persona segun json ConfigAperturaPersona
- let infoPersonasFilter=infoPersonas.filter(y=>
-(ConfigAperturaPersona.filter(z=>z["TIPO"]=="CLIENTE").map(x=> x["EMP_CODI"]+x["CENCO1_CODI"]).includes(empresa+y["CENCO1_CODI"]))
-||(ConfigAperturaPersona.filter(z=>z["TIPO"]=="EMPRESA").map(x=>x["EMP_CODI"]).includes(parseInt(empresa)))
-
-
- )
-
-
-
-await generaFiles(infoPersonasFilter,'NOMBRES_CENCO2_CODI',dirDestinoPersona,empresa,datosNominaHeader)
-
-
-
-
-
-}
-
+    }else{
+      fs.mkdirSync(carpetaBurst);
+    }
+  
+      let rutsEncontrados
+  
+      try {
+        console.log("EEEEE")
+      //	let uploadFileName='C:/Users/jpierre/Documents/NodeProjects/liquidaciones-sueldo/api-server/CotizacionesPersonal.pdf'
+        rutsEncontrados = await getRutsOfFile(uploadFileName,empresa)
+        
+    var tablaMapPersonas = (await generaMapPersonas(rutsEncontrados, empresa,mes))//.slice(0,5)  //para control de cantidad de la cantidad de archivos que se generaran ***********
+     // console.log(tablaMapPersonas)
+    } catch (e) {
+      console.log("no tiene formato", e)
+    
+    
+    }
+  
+    let child = await  new Promise ( (resolve,reject)=>{
+      // exec('pdftk ' + uploadFileName + ' cat ' + pagesCC + ' output ' + path_output_base + centro_costo + '.pdf',
+     // exec('pdftk ' + uploadFileName + ' cat ' + pagesCC + ' output ' + FileServer.convertPath(dirDestino+'\\' + centro_costo) + "-["+empresa+"]"+'-PREVIRED['+Utils.getDateFormat().substr(0,10)+']'+'.pdf',
+      
+     //page_%01d tien 1,page_%02d tiene 01,page_%03d tien 001 y asi sucesivamente
+    //exec('pdftk ' + uploadFileName + ' cat ' + pagesCC + ' output ' + FileServer.convertPath(dirDestino+'\\' + centro_costo) + "-["+empresa+"]"+'-PREVIRED['+Utils.getDateFormat().substr(0,10)+']'+'.pdf',
+  
+     exec('pdftk  ' + uploadFileName + ' burst output  pdfBurstPrevired/page_%01d.pdf',
+    
+       function (error, stdout, stderr) {
+         console.log('stdout: ' + stdout);
+         console.log('stderr: ' + stderr);
+       
+         
+      
+         if (error !== null) {
+           console.log('exec error: ' + error);
+    
+         }
+        
+         //
+          //entrega data para validacion
+         resolve()
+    
+       });
+      });
+  /*
+    {
+      RUT: '016.877.086-3',
+      RUT_ID: 16877086,
+      PAGINA: 1541,
+      FICHA: 'CARONOS389',
+      CENCO2_CODI: '165-012',
+      NOMBRES: 'CLAUDIA ANDREA'
+    },
+    
+  */
+  let processInfo={
+    name:"PREVIRED",
+    type:77,
+    referencialDate:mes.substr(0,7)+'-30',
+    monthInsacom: mes.substr(5,2),
+    yearInsacom:mes.substr(0,4)
+  
+  }
+  console.log(processInfo)
+       console.log("termino burst (separa todo en paginas")
+       
+       let cantIteraciones = tablaMapPersonas.length
+      // let cantIteraciones =2
+       for (let i = 0; i < cantIteraciones; i++) {
+       let personaFile= tablaMapPersonas[i]
+       //filename=testPdfBurst/page_%01d.pdf
+       let filename=carpetaBurst+'/page_'+personaFile["PAGINA"]+'.pdf'
+       console.log("pagina buscada"+filename)
+        var buffer = fs.readFileSync(filename);
+        let base64=Buffer.from(buffer).toString('base64')
+        if(base64){
+        
+        console.log("se recibio archvio")
+  
+  
+        let response=await FileProjectController.fileProjectPost(processInfo,personaFile,base64)
+        console.log('response',JSON.stringify(response))
+  
+            //envia evento de completitud del archivo
+          StatusPrevired.msgs[0] = personaFile.FICHA
+          StatusPrevired.percent =(( i + 1) / cantIteraciones* 100)
+          io.emit('getStatusPrevired', StatusPrevired)
+        
+      }else{ 
+        console.log("error no se recibio archivo")
+      }
+  
+       // let response=await FileProjectController.fileProjectPost(null,base64)
+        
+       }
+      // var buffer = fs.readFileSync(filename);
+    
+     console.log("todos los trabajos terminados")
+  
+  
+  }
+  
 
 
 
